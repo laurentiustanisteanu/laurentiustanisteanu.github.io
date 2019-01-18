a60e93ad399ebf6c311703d0c7e53b386db026e5
Laurentiu Stanisteanu <laurentiustanisteanu@gmaIL.COM>
2019-01-18 23:36:31 +0200

    q

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Laurentiu Stanisteanu.", "A Gis Specialist.", "Hydrologist.", "Freelancer.", "Photographer"],
+      strings: ["Laurentiu Stanisteanu.", "A Gis Specialist.", "A Hydrographic Surveyor.", "A Hydrological Modeller.", "A Hydrologist "],
       typeSpeed: 100,
       loop: true,
     });
